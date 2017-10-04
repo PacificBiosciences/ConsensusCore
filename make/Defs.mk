8fb7f8689d0b878693ea433d94ff3eed9523320d
David Seifert <dseifert@pacificbiosciences.com>
2017-10-04 11:08:17 -0700

    Use C++14 by default
    * Provide fallback option for C++98,
      but TAG uses C++14 everywhere now.
    * Use `CPP_STD` identifier instead of
      `CPP_ABI`, which is misleading, as the
      C++ ABI is not determined by the
      `-std=` setting.

--- make/Defs.mk
+++ make/Defs.mk
@@ -47,10 +47,10 @@ endif
 
 ifeq ($(GXX),clang++)
     CXX_FLAGS           = $(GXX_FLAGS) $(CXX_OPT_FLAGS) -msse3 -fPIC -Qunused-arguments -fno-omit-frame-pointer
-    CXX_STRICT_FLAGS    = $(GXX_FLAGS) $(CXX_FLAGS) -pedantic -std=$(CPP_ABI) -Wall
+    CXX_STRICT_FLAGS    = $(GXX_FLAGS) $(CXX_FLAGS) -pedantic -std=$(CPP_STD) -Wall
 else
     CXX_FLAGS           = $(CXX_OPT_FLAGS) $(CXX_EXTRA_ARGS) -msse3 -fPIC -fno-omit-frame-pointer
-    CXX_STRICT_FLAGS    = $(CXX_FLAGS) -pedantic -std=$(CPP_ABI) -Wall
+    CXX_STRICT_FLAGS    = $(CXX_FLAGS) -pedantic -std=$(CPP_STD) -Wall
 endif
 
 CXX             = $(CCACHE) $(GXX) $(MACHINE) $(CXX_FLAGS) $(INCLUDES) -isystem $(BOOST)
